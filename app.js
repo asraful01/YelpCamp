85e456310bf67fd6cb330719a329a0ff441aca63
asraful01 <asraful.babul53@qmail.cuny.edu>
2020-08-18 20:39:23 -0400

    installing passport authentication, authenticating users

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express       = require("express"),
       port          = 3000,
       bodyParser    = require("body-parser"),
       mongoose      = require('mongoose'),
+      passport      = require("passport"),
+      LocalStrategy = require ("passport-local"),
       Campground    = require("./models/campground"),
       Comment       = require("./models/comment"),
+      User          = require("./models/user"),
       seedDB        = require("./seeds")
       
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,6 +21,19 @@ mongoose.connect('mongodb://localhost:27017/campground', {
 .then(() => console.log('Connected to DB!'))
 .catch(error => console.log(error.message));
 seedDB();
+
+//passport config
+app.use(require("express-session")({
+    secret:"once again",
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/",(req,res)=>{
     res.render("landing");
@@ -107,7 +123,26 @@ app.post("/campgrounds/:id/comments",(req,res)=>{
     
 })
 
+//AUTH ROUTES
 
+//SHOW register form
+app.get("/register",(req,res)=>{
+    res.render("register");
+})
+//handle sign up 
+app.post("/register",(req,res)=>{
+   var newUser=new User({username:req.body.username})
+   User.register(newUser,req.body.password,(err,user)=>{
+       if(err){
+           console.log(err);
+           return res.render("register");
+       }
+       passport.authenticate("local")(req, res,()=>{
+        res.redirect("/campgrounds");
+       })
+    
+   })
+});
 
 app.listen(port,()=>{
     console.log("app running");
